6d59745660c52adfd373bb36927580e6aca95bbb
Poojitha2510 <banothpoojitha435@gmail.com>
2026-03-05 23:04:15 +0530

    intial change

---
+++ 05MarchBatch/first.py
@@ -0,0 +1,54 @@
+print("Hello World")
+name="Poojitha"
+surname="Banoth"
+age=20
+#print(age+10)
+#print("Hi,My name is",name,"and my surname is",surname,"and my age is",age)
+
+#operaters(+,-,*,/,%)
+"""print(9+2)
+print(9-2)
+print(9*2)
+print(9/2)#return the quotient value
+print(9//2)
+print(14%7)#returns remainder value
+print(5001%1000000001)#5001"""
+
+#Boolean values: hold only two values (True or False)
+
+#comparision operater:(>,<,>=,<=,==,!=)operater to compare two values
+'''a=10
+b=20
+print(a==b)#comparing the both values or equal  or not
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)'''
+
+#Logical operaters(and,or,not):works on binary operaters
+'''print(True and True)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(False or True)
+print(False or False)'''
+
+#input function
+input("provide input")
+age=input("enter your age")# stores value as string
+print(age)
+#print(age+20)
+print(type(age))
+
+# to conver it into integer
+age=int(input("enter your age"))
+print(type(age))
+
+
+
+
+
+
+
+
